fb92d7f10fb110ba4f7a097fb6200fa6bfd88a8b
David Ingram <david@ingramaviation.com>
2019-08-15 19:05:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,26 +1,17 @@
 def roll_call_dwarves(dwarves)
-  # Your code here
    dwarves.each_with_index{|element, index| puts "#{index + 1} #{element}"}
-  
 end
   
 
 def summon_captain_planet(planeteer_calls)
-   # Your code here
    planeteer_calls.map{|element| element.capitalize + '!'}
-   
 end
 
 def long_planeteer_calls(calls_long)
-  # Your code here
   calls_long.any?{|element| element.length > 4}
-  
 end
 
 def find_the_cheese(contains_cheddar)
-  # the array below is here to help
-  
   cheese_types = ["cheddar", "gouda", "camembert"]
   contains_cheddar.find{|element| cheese_types.include?(element)}
-  
 end
